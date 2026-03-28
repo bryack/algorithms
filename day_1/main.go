968f4cdfee3b3e85d76f58ecd600fc1f5e8c67fd
bryacka <bryacka@gmail.com>
2026-03-28 21:36:03 +0300

    1

--- day_1/main.go
+++ day_1/main.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
-func main() {
-	for i := 1; i <= 10; i++ {
+func multiply() {
+	for i := 0; i <= 10; i++ {
 		fmt.Printf("7 x %d = %d\n", i, i*7)
 	}
+}
+
+func exponent() {
+	e := 2
+	for i := 1; i <= 10; i++ {
+		fmt.Printf("2^%d = %d\n", i, e)
+		e *= 2
+	}
+}
+
+func main() {
+	multiply()
+	exponent()
 }
