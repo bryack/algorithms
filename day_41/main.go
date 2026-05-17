9a80fea542432aa097cb63fef4174e4372937264
bryacka <bryacka@gmail.com>
2026-05-17 09:46:17 +0300

    add removeDuplicates for LeetCode 26
    - Fast & slow pointers with in-place overwrite
    - O(n) time, O(1) space
    - Returns length of unique prefix

--- day_41/main.go
+++ day_41/main.go
@@ -92,3 +92,14 @@ func maxArea(height []int) int {
 	}
 	return maxArea
 }
+
+func removeDuplicates(nums []int) int {
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != nums[j] {
+			j++
+			nums[j] = nums[i]
+		}
+	}
+	return j + 1
+}
