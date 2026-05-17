0c6a0bf7914becbffe2f010e03065c567c84ec2e
bryacka <bryacka@gmail.com>
2026-05-17 09:08:47 +0300

    add twoSum for LeetCode 167 (Two Sum II)
    - Two-pointer convergence on sorted array
    - Returns 1-based indices per problem spec
    - O(n) time, O(1) space

--- day_41/main.go
+++ day_41/main.go
@@ -22,3 +22,20 @@ func isPalindrome(s string) bool {
 	}
 	return true
 }
+
+func twoSum(nums []int, target int) []int {
+	i, j := 0, len(nums)-1
+
+	for i < j {
+		sum := nums[i] + nums[j]
+		if sum == target {
+			return []int{i + 1, j + 1}
+		}
+		if sum > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return nil
+}
