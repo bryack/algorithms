8627e9730ce64d26cfbd80180b8812013d21c77c
bryacka <bryacka@gmail.com>
2026-05-17 09:34:32 +0300

    add maxArea for LeetCode 11 (Container With Most Water)
    - Two pointers from both ends
    - Moves pointer with smaller height to maximize area
    - O(n) time, O(1) space

--- day_41/main.go
+++ day_41/main.go
@@ -74,3 +74,21 @@ func threeSum(nums []int) [][]int {
 	}
 	return res
 }
+
+func maxArea(height []int) int {
+	maxArea := 0
+	i, j := 0, len(height)-1
+
+	for i < j {
+		if height[i] < height[j] {
+			currentArea := height[i] * (j - i)
+			maxArea = max(maxArea, currentArea)
+			i++
+		} else {
+			currentArea := height[j] * (j - i)
+			maxArea = max(maxArea, currentArea)
+			j--
+		}
+	}
+	return maxArea
+}
