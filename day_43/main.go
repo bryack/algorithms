d9ad17435bbc2bfd5875f59ada224f20d1ec2003
bryacka <bryacka@gmail.com>
2026-05-19 15:50:40 +0300

    LC 1343 Number of Sub-arrays with Avg >= Threshold
    Add numOfSubarrays() to day_43/main.go.
    - Fixed-size sliding window counting valid subarrays
    - O(n) time, O(1) space
    - Zero Hints warm-up repetition

--- day_43/main.go
+++ day_43/main.go
@@ -19,3 +19,23 @@ func findMaxAverage(nums []int, k int) float64 {
 	}
 	return float64(res) / float64(k)
 }
+
+func numOfSubarrays(arr []int, k int, threshold int) int {
+	begin := 0
+	w_state := 0
+	res := 0
+
+	for end := range arr {
+		w_state += arr[end]
+		w_size := end - begin + 1
+
+		if w_size == k {
+			if w_state/k >= threshold {
+				res++
+			}
+			w_state -= arr[begin]
+			begin++
+		}
+	}
+	return res
+}
